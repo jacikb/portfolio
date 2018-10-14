1e3a07415c7a33fb744bc7afe29db355a3fe93ea
Jacek Brzezinski <jacik@toya.net.pl>
2018-10-14 17:56:10 +0200

    V1.2 add RouteService. isAuthor

--- src/AppBundle/Form/RegistrationType.php
+++ src/AppBundle/Form/RegistrationType.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 // src/AppBundle/Form/RegistrationType.php
 
@@ -35,42 +34,5 @@ class RegistrationType extends AbstractType
     }
 
 }
-=======
-<?php
-// src/AppBundle/Form/RegistrationType.php
 
-namespace AppBundle\Form;
-use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\FormBuilderInterface;
-
-class RegistrationType extends AbstractType
-
-{
-    public function buildForm(FormBuilderInterface $builder, array $options)
-
-    {
-        $builder->add('firstName');
-        $builder->add('lastName');
-    }
-
-    public function getParent()
-
-    {
-        return 'FOS\UserBundle\Form\Type\RegistrationFormType';
-    }
-
-    public function getBlockPrefix()
-
-    {
-        return 'app_user_registration';
-    }
-
-    public function getName()
-
-    {
-        return $this->getBlockPrefix();
-    }
-
-}
->>>>>>> 8e271d3cf4e8ad276868f3b207823c441c349c6a
 ?>
