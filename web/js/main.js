77c2b38ef46910596db8c7361cec32df775d80a1
Jacek Brzezinski <jacik@toya.net.pl>
2018-10-15 10:17:25 +0200

    V1.2 link on index

--- web/js/main.js
+++ web/js/main.js
@@ -1,18 +1,6 @@
-<<<<<<< HEAD
 /**
  * Created by Jacik on 2018-10-13.
  */
 $( document ).ready(function() {
-
-
-
-=======
-/**
- * Created by Jacik on 2018-10-13.
- */
-$( document ).ready(function() {
-
-
-
->>>>>>> 8e271d3cf4e8ad276868f3b207823c441c349c6a
+    $(".show").fadeIn(800);
 });
